335ddd90602b3e5353b2a25832a56934410486d7
dave-f <dave.footitt@gmail.com>
2020-08-15 22:36:00 +0100

    Add customizable header and footer

--- baff.el
+++ baff.el
@@ -1,22 +1,44 @@
-;;
-;; baff
-;; byte array from file
+;;; -*- lexical-binding: t; -*-
+;;;
+;;; baff.el --- Create a byte array from a file
 
 (require 'f)
 
+(defgroup baff nil
+  "Make a byte array from a file")
+
+(defcustom baff-header-function '(lambda (arg) (insert "// sha256: " (secure-hash 'sha256 arg) "\n\nstd::array<uint8_t," (number-to-string (length arg)) "> bytes = {\n"))
+  "Function to run before any bytes are inserted."
+  :group 'baff)
+
+(defcustom baff-footer-function '(lambda (arg) (insert "\n};"))
+  "Function to run after all bytes have been inserted."
+  :group 'baff)
+
+(defcustom baff-indent-function '(lambda () (insert "    "))
+  "Function to indent each line."
+  :group 'baff)
+
+(defcustom baff-bytes-per-line 16
+  "Number of bytes per line before a line break."
+  :group 'baff)
+
 (defun baff (arg)
-  (interactive "FFile: ")
+  (interactive "FFile to insert: ")
   (if (not (f-file-p arg))
-      (error "File error"))
-  (switch-to-buffer (get-buffer-create "*baff*"))
-  (erase-buffer)
-  (let ((bytes (string-to-list (f-read-bytes arg)))
-        (count 0))
-    (insert "std::array<uint8_t," (number-to-string (length bytes)) "> bytes = {\n")
+      (error "File open error"))
+  (let* ((unibytes (f-read-bytes arg))
+         (bytes (string-to-list unibytes))
+         (count 0))
+    (switch-to-buffer (get-buffer-create "*baff*"))
+    (erase-buffer)
+    (funcall baff-header-function unibytes)
+    (funcall baff-indent-function)
     (cl-loop for i in bytes do
              (setq count (1+ count))
              (insert (format "0x%02x" i) (if (= count (length bytes)) "" ","))
-             (when (= (% count 16) 0)
-               (insert "\n")))
-  (insert "\n};"))
+             (when (= (% count baff-bytes-per-line) 0)
+               (insert "\n")
+               (funcall baff-indent-function)))
+  (funcall baff-footer-function unibytes))
   t)
